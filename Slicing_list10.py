7e4b078d3a2fdda45bd44ee81943f8e3a8ce5d73
Umidjon <mrumidjon2020@gmail.com>
2022-03-30 23:02:35 +0500

    A list of several elements is given. Return all items except n elements from the beginning. 02

--- Slicing_list10.py
+++ Slicing_list10.py
@@ -1,6 +1,6 @@
 def main(list1, n):
     """
-    A list of several elements is given. Return all elements in reverse order except n elements from the beginning.
+    A list of several elements is given.  Return all elements in reverse order except n elements from the beginning.
     Args:
         list1(list): parameter
         n(int): parameter
