f239ce300e6da95864fac13924abdb6f78780405
Umidjon <mrumidjon2020@gmail.com>
2022-03-30 22:57:25 +0500

    A list of several elements is given. Return all items except n elements from the beginning. 01

--- Slicing_list09.py
+++ Slicing_list09.py
@@ -1,4 +1,4 @@
-def main(list1,n):
+def main(list1, n):
     """
     A list of several elements is given. Return all items except n elements from the beginning.
     Args:
@@ -7,4 +7,4 @@ def main(list1,n):
     Returns:
         list: return answer.
     """
-    return
+    return list1[n:]
